e3204d501eddf9cdd7852c81e857f69c754107ac
jerryxu <13048699388@163.com>
2017-05-10 01:34:04 +0800

    update the monogo url

--- server/db/mongodb-connection.js
+++ server/db/mongodb-connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/monitor');
+mongoose.connect('mongodb://127.0.0.1/monitor');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
